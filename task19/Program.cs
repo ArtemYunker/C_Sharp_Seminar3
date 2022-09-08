2a13bb4645e68067dbd3be424b1f168d4ebe9b88
Artem Yunker <art-yunker@yandex.ru>
2022-09-08 19:03:10 +0200

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//  является ли оно палиндромом.
+
+
+int number = new Random().Next(9999, 99999);
+Console.WriteLine(number);
+
+string str = number.ToString();
+if (str[0] == str[4] && str[1] == str[3])
+{
+    Console.WriteLine("Число палиндром ");
+}
+else
+{
+    Console.WriteLine("Число не палиндром ");
+}
